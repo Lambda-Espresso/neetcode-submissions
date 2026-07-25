6d9e1264bd7ad4f1047095c5dd16607af6c32608
Lambda Espresso <deekayndkn@gmail.com>
2026-07-25 15:28:59 +0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.add(')');
+            }
+            else if (c == '{') {
+                stack.add('}');
+            }
+            else if (c == '[') {
+                stack.add(']');
+            } 
+            else {
+                if (stack.isEmpty()) {
+                    return false;
+                } 
+                if (c != stack.pop()) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
