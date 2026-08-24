61a6790dbb6ccea7e1161104220ba664269d1c67
Lambda <deekayndkn@gmail.com>
2026-08-24 13:17:44 +0700

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length() + "#" + str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> list = new ArrayList<>();
+        int len = str.length();
+        int i = 0;
+        while (i < len) {
+            int strLen = str.charAt(i) - '0';
+            while (++i < len && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                strLen = strLen * 10 + (str.charAt(i) - '0');
+            }
+            if (strLen == 0) {
+                list.add("");
+                i++;
+            }
+            else list.add(str.substring(++i, i += strLen));
+        }
+        return list;
+    }
+}
